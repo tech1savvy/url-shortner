8ba73108e649d60ff934e1f34575d25832e3cd84
tech1savvy <amankumar010604@gmail.com>
2025-08-06 23:34:21 +0530

    feat: Implement user interface and static routes

---
+++ features/views/static.routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const URL = require("../url/url.model");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  if (!req.user) {
+    return res.redirect("/login");
+  }
+  const allUrls = await URL.find({ createdBy: req.user._id });
+  res.render("home", { urls: allUrls });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+module.exports = router;
